6c32182626199e4a8a0bece40ea111fbc554916b
Ramananpydev <ramananpydev@gmail.com>
2025-12-22 20:07:36 +0530

    'add'

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,18 @@ function App() {
     { id: 2, image: '/TereIshkMei.jpg', heading: 'TereIshkMei', p: 'UA16+ Tamil' },
     { id: 3, image: '/Padayappa.jpg', heading: 'Padayappa', p: 'UA Tamil' },
     { id: 4, image: '/Kombuseevi.jpg', heading: 'Kombuseevi', p: 'UA13+ Tamil' },
-    { id: 5, image: '/45.jpg', heading: '45', p: 'UA16+ Tamil, English +2 languages' },
-    { id: 6, image: '/SpongeBob.jpg', heading: 'SpongeBob', p: 'UA16+ Tamil, English +2 languages' },
-    { id: 7, image: '/dhuran.jpg', heading: 'Dhurandhar', p: 'A Hindi' },
-    { id: 8, image: '/webury.jpg', heading: 'We Bury The Dead', p: 'UA16+  English ' },
-    { id: 9, image: '/SBM.jpg', heading: 'SBM', p: 'UA16+ Tamil' },
-    { id: 10, image: '/Sirai.jpg', heading: 'Sirai', p: 'UA16+ Tamil' },
-    { id: 11, image: '/Kalamkaval.jpg', heading: 'Kalamkaval', p: 'UA16+ Malayalam' },
-    { id: 12, image: '/Champion.jpg', heading: 'Champion', p: 'UA16+ Hindi' },
-    { id: 12, image: '/Anaconda.webp', heading: 'Anaconda', p: 'UA16+ Tamil, English +2 languages' },
-    { id: 12, image: '/Zootopia.jpg', heading: 'Zootopia', p: 'UA7+ Tamil, English ' },
+    { id: 5, image: '/SpongeBob.jpg', heading: 'SpongeBob', p: 'UA16+ Tamil, English +2 languages' },
+    { id: 6, image: '/dhuran.jpg', heading: 'Dhurandhar', p: 'A Hindi' },
+    { id: 7, image: '/webury.jpg', heading: 'We Bury The Dead', p: 'UA16+  English ' },
+    { id: 8, image: '/SBM.jpg', heading: 'SBM', p: 'UA16+ Tamil' },
+    { id: 9, image: '/Sirai.jpg', heading: 'Sirai', p: 'UA16+ Tamil' },
+    { id: 10, image: '/Kalamkaval.jpg', heading: 'Kalamkaval', p: 'UA16+ Malayalam' },
+    { id: 11, image: '/BhaBhaBa.jpg', heading: 'Bha BHa BA', p: 'UA13+ Malayalam' },
     { id: 12, image: '/akhada.jpg', heading: 'Akhada', p: 'UA16+ Tamil, Telugu' },
-    { id: 12, image: '/BhaBhaBa.jpg', heading: 'Bha BHa BA', p: 'UA13+ Malayalam' }
+    { id: 13, image: '/Zootopia.jpg', heading: 'Zootopia', p: 'UA7+ Tamil, English ' },
+    { id: 14, image: '/45.jpg', heading: '45', p: 'UA16+ Tamil, English +2 languages' },
+    { id: 15, image: '/Anaconda.webp', heading: 'Anaconda', p: 'UA16+ Tamil, English +2 languages' },
+    { id: 16, image: '/Champion.jpg', heading: 'Champion', p: 'UA16+ Hindi' }
   ]
 
   return (
